d58107dff8cbbce5cab2ac8f1f5b8bbcd8f8bc1f
gasSolid <35830863+gasSolid@users.noreply.github.com>
2020-09-16 18:02:37 +0800

    Add files via upload
    添加matplot配置文件，以及坐标轴变粗，加大

--- postprocessing_PDF/post_Flu.py
+++ postprocessing_PDF/post_Flu.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import math
 
 def calc_PDF_Kn (data, n_interval):
@@ -9,7 +10,6 @@ def calc_PDF_Kn (data, n_interval):
     calc the PDF of data
     '''
     data.sort()
-    count = 0
     for i in range(len(data)):
         if data[i] > 0:
             x_min = math.log10(data[i])
@@ -43,20 +43,22 @@ def calc_PDF_Kn (data, n_interval):
 
     x_ret = []
     y_ret = []
+    accumu_PDF = 0.0
     for i in range(len(x_axi)):
         if PDF[i] == 0:
             continue
         x_ret.append(x_axi[i])
         y_ret.append(PDF[i])
+	if (x_axi[i]<0.1):
+            accumu_PDF += PDF[i]
 
-    return x_ret, y_ret
+    return x_ret, y_ret, accumu_PDF
 
 def calc_PDF (data, n_interval):
     '''
     calc the PDF of data
     '''
     data.sort()
-    count = 0
     for i in range(len(data)):
         if data[i] > 0:
             x_min = math.log10(data[i])
@@ -89,13 +91,16 @@ def calc_PDF (data, n_interval):
 
     x_ret = []
     y_ret = []
+    accumu_PDF = 0.0;
     for i in range(len(x_axi)):
         if PDF[i] == 0:
             continue
         x_ret.append(x_axi[i])
         y_ret.append(PDF[i])
+	if (x_axi[i]>1.0):
+            accumu_PDF += PDF[i]
 
-    return x_ret, y_ret
+    return x_ret, y_ret, accumu_PDF
 
 #reading data
 file = open("all.data","rb")
@@ -123,87 +128,127 @@ for line in file.readlines():
     Is.append(x5)
 
 # calc PDF
-x_axi_Ma_S, PDF_Ma_S = calc_PDF(Ma_Savage,n_interval)
-x_axi_Ma_dpm, PDF_Ma_dpm = calc_PDF(Ma_dpm,n_interval)
-x_axi_Kn_f, PDF_Kn_f = calc_PDF_Kn(Kn_frac,n_interval)
-x_axi_Kn_M, PDF_Kn_M = calc_PDF_Kn(Kn_Mag_us,n_interval)
-x_axi_Kn_g, PDF_Kn_g = calc_PDF_Kn(Kn_gran,n_interval)
-x_axi_Is, PDF_Is = calc_PDF_Kn(Is,n_interval)
+x_axi_Ma_S, PDF_Ma_S, accumu_PDF_Ma_s = calc_PDF(Ma_Savage,n_interval)
+x_axi_Ma_dpm, PDF_Ma_dpm, accumu_PDF_Ma_d = calc_PDF(Ma_dpm,n_interval)
+x_axi_Kn_f, PDF_Kn_f, accumu_PDF_Kn_f = calc_PDF_Kn(Kn_frac,n_interval)
+x_axi_Kn_M, PDF_Kn_M, accumu_PDF_Kn_M = calc_PDF_Kn(Kn_Mag_us,n_interval)
+x_axi_Kn_g, PDF_Kn_g, accumu_PDF_Kn_g = calc_PDF_Kn(Kn_gran,n_interval)
+x_axi_Is, PDF_Is, accumu_PDF_Is = calc_PDF_Kn(Is,n_interval)
+
+print "P(Knf<0.1) = ", accumu_PDF_Kn_f 
+print "P(Knv<0.1) = ", accumu_PDF_Kn_M 
+print "P(Kng<0.1) = ", accumu_PDF_Kn_g 
+print "P(Is<0.1) = ", accumu_PDF_Is
+print "P(Ma_Sa>1) = ", accumu_PDF_Ma_s
+print "P(Ma_dpm>1) = ", accumu_PDF_Ma_d
 
 #import csv
 #with open("fw_htc.csv","w") as outfile:
 #    writer = csv.writer(outfile,delimiter = '\t')
 #    writer.writerows(zip(yData1))
 
-fig = plt.figure(num=1, figsize=(16,12))
+fig = plt.figure(num=1, figsize=(16,14))
 plt.rc('text',usetex=True)
-plt.rc('font',family='serif')
+mpl.rc('font', family='serif', weight = 'bold')
 
-#plt.title('FCCUg0.06', size=14)
+plt.title('FCCUg0.06', size=14)
+
 #plt.xlabel("",size = 14)
 #plt.ylabel('PDF', size=14)
-#laTex example
-#plt.xlabel(r"$\theta^ \circ$",size = 14)
 
+#set tick size
+#mpl.rcParams['xtick.major.size'] = 20
 
-plt.subplot(3,2,1)
+ax = plt.subplot(3,2,1)
 plt.scatter(x_axi_Kn_f, PDF_Kn_f, color='g',  marker='>')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlim(5e-6, 1e1)
 plt.ylim(1e-5, 1e-1)
-plt.xlabel("Kn_{frac}",size = 14)
-plt.ylabel('Probability distribution', size=14)
+plt.xlabel(r"$\textbf{Kn_{frac}}$",weight = 'bold', size = 14)
+plt.ylabel(r"$\textbf{Probability distribution}$", size=14)
+plt.text(1e-5,0.03 , r"$\textbf{P = 0.83}$", size = 14)
+#tick line size
+plt.setp(ax.yaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.yaxis.get_minorticklines(), 'markeredgewidth', 1)
+plt.setp(ax.xaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.xaxis.get_minorticklines(), 'markeredgewidth', 1)
+ax.xaxis.get_label().set_weight('black')
+#plt.setp(ax.yaxis.get_label(), 'weight', 'bold')
 
-plt.subplot(3,2,2)
+ax = plt.subplot(3,2,2)
 plt.scatter(x_axi_Kn_M, PDF_Kn_M, color='c', marker='^')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlim(5e-6, 1e1)
 plt.ylim(1e-5, 1e-1)
-plt.xlabel("Kn_{vel}",size = 14)
+plt.xlabel(r"$\textbf{Kn_{vel}}$",size = 14)
+plt.text(1e-5,0.03 , r"$\textbf{P = 0.88}$", size = 14)
+plt.setp(ax.yaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.yaxis.get_minorticklines(), 'markeredgewidth', 1)
+plt.setp(ax.xaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.xaxis.get_minorticklines(), 'markeredgewidth', 1)
 
-plt.subplot(3,2,3)
+ax = plt.subplot(3,2,3)
 plt.scatter(x_axi_Kn_g, PDF_Kn_g, color='m', marker='+')
 plt.xscale('log')
 plt.yscale('log')
-plt.xlim(5e-5, 1e1)
+plt.xlim(1e-4, 1e1)
 plt.ylim(1e-5, 1e-1)
-plt.xlabel("Kn_{gran}",size = 14)
-plt.ylabel('Probability distribution', size=14)
+plt.xlabel(r"$\textbf{Kn_{gran}}$",size = 14)
+plt.ylabel(r"$\textbf{Probability distribution}$", size=14)
+plt.text(2e-4,0.03 , r"$\textbf{P = 0.89}$", size = 14)
+plt.setp(ax.yaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.yaxis.get_minorticklines(), 'markeredgewidth', 1)
+plt.setp(ax.xaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.xaxis.get_minorticklines(), 'markeredgewidth', 1)
 
-plt.subplot(3,2,4)
+ax = plt.subplot(3,2,4)
 plt.scatter(x_axi_Is, PDF_Is, color='navy', marker='x')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlim(6e-4, 1e1)
 plt.ylim(1e-5, 1e-1)
-plt.xlabel("I_{s}",size = 14)
+plt.xlabel(r"$\textbf{I_{s}}$",size = 14)
+plt.text(2e-3,0.03 , r"$\textbf{P = 0.94}$", size = 14)
+plt.setp(ax.yaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.yaxis.get_minorticklines(), 'markeredgewidth', 1)
+plt.setp(ax.xaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.xaxis.get_minorticklines(), 'markeredgewidth', 1)
 
-plt.subplot(3,2,5)
+ax = plt.subplot(3,2,5)
 plt.scatter(x_axi_Ma_S, PDF_Ma_S, color='r',  marker='o', label=None)
 plt.xscale('log')
 plt.yscale('log')
 plt.xlim(1e-2, 1e3)
 plt.ylim(1e-5, 1e-1)
-plt.ylabel('Probability distribution', size=14)
-plt.xlabel("Ma",size = 14)
-#I = plt.legend(loc='best')
-#I.draw_frame(False)
-plt.text(2,0.03 , "Ma (Marchisio and Fox, 2013)", size = 14)
-#plt.legend(frameon = False)
+plt.ylabel(r"$\textbf{Probability distribution}$", size=14)
+plt.xlabel(r"$\textbf{Ma}$",size = 14)
+plt.text(0.02,0.03 , r"$\textbf{P = 0.63}$", size = 14)
+plt.text(10,0.03 , r"$\textbf{Ma (Savage, 1988)}$", size = 14)
+plt.setp(ax.yaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.yaxis.get_minorticklines(), 'markeredgewidth', 1)
+plt.setp(ax.xaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.xaxis.get_minorticklines(), 'markeredgewidth', 1)
 
-plt.subplot(3,2,6)
+ax = plt.subplot(3,2,6)
 plt.scatter(x_axi_Ma_dpm, PDF_Ma_dpm, color='b',  marker='<', label = None)
 plt.xscale('log')
 plt.yscale('log')
 plt.xlim(5e-2, 1e4)
-plt.ylim(1e-5, 1e-1)
-plt.xlabel("Ma",size = 14)
+plt.ylim(1e-5, 1.3e-1)
+plt.xlabel(r"$\textbf{Ma}$",size = 14)
+plt.text(0.1,0.035 , r"$\textbf{P = 0.99}$", size = 14)
+plt.text(10,0.035 , r"$\textbf{Ma (Marchisio and Fox, 2013)}$", size = 14)
+plt.setp(ax.yaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.yaxis.get_minorticklines(), 'markeredgewidth', 1)
+plt.setp(ax.xaxis.get_ticklines(), 'markeredgewidth', 2)
+plt.setp(ax.xaxis.get_minorticklines(), 'markeredgewidth', 1)
 #I = plt.legend(loc='best')
 #I.draw_frame(False)
 #plt.legend(frameon = False)
-plt.text(200,0.03 , "Ma (Savage, 1988)", size = 14)
+
+
 
 #fig.set_ylabel('Probability distribution', size=14)
 plt.show()
